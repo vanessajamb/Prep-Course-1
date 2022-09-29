74c166316318144aeebff8ac2a6bfc7550b66606
Ulises Jimenez Luna <ulises@MacBook-Air-de-Ulises.local>
2022-09-29 09:03:16 -0500

    sexto commit

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -157,8 +157,14 @@ function agregarMetodoCalculoDescuento (producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-
+  
+  producto.calcularPrecioDescuento = function() {
+    return this.precio - ( this.precio * this.porcentajeDeDescuento );
+  };
+  return producto;
 }
+     
+    
 
 // No modificar nada debajo de esta línea
 // --------------------------------
